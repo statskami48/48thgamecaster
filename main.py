4997d6b26ae8ec566c75f4fd2b75a91b3c531653
Stats 48TH <stats.kami48@gmail.com>
2023-05-18 19:18:56 +0700

    init

--- main.py
+++ main.py
@@ -12,3 +12,12 @@ st.code(code, language='python')
 show_btn = st.button("Show code")
 if show_btn:
     st.code("Button has already clicked")
+
+age_inp = st.number_input("Input your age")
+st.markdown(f"Your age is {age_inp}")
+
+
+st.markdown("# ggg")
+text_inp = st.text_input("Input")
+word_tokenize = "|".join(text_inp.split())
+st.markdown(f"{word_tokenize}")
