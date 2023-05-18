9bad0e1b26e9104c1b478433c3eddfd757850f96
Stats 48TH <stats.kami48@gmail.com>
2023-05-18 23:19:15 +0700

    init

---
+++ pages/1_Summary.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Summary : Statistics 48TH Game Caster", page_icon="🌟", layout="centered")
+st.title("48TH Game Caster")
+st.subheader("Please Like & follow")
+st.write("https://www.facebook.com/48THGameCaster")
+
+st.markdown("Summary data")
